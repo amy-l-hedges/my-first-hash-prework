4ab9b4d6270bff763fdf697a2df7de9effffd12f
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-15 23:29:41 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,13 +7,16 @@ def shipping_manifest
  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
-#def retrieval
+def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
+return shipping_manifest[2]
+end
+ 
+ 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 #end
