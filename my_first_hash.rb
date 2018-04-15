4da3231b82caf59cb6606d1d902efe10efeaac7d
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-15 23:34:04 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,26 +15,14 @@ def retrieval
                  }
 shipping_manifest["oil paintings"]
 end
- 
- 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
-#end
-
-#def adding
+def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
-#end
+shipping_manifest["muskets" => 2]
+shipping_manifest["gun powder" => 4]
+return shipping_manifest
+end
